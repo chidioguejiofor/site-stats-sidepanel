d4e0a1994e59ccdbc09ee7d79f47adacba01f0a5
Chidiebere Ogujeiofor <chidioguejiofor@gmail.com>
2025-04-27 01:17:07 +0100

    WIP

---
+++ backend/test_integration_tests.py
@@ -0,0 +1,10 @@
+from main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_root():
+    response = client.get('/')
+    assert response.json() == {'Hello': "you"}
+
