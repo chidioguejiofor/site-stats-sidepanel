f27d385a603aeb54ca59c8820b4a8c2728a49e8a
chidioguejiofor <chidioguejiofor@gmail.com>
2025-04-27 02:07:26 +0100

    feat: adds FastAPI setup and endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,40 @@
 from typing import Union
-
+from pydantic import BaseModel
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
+db = []
+class PageVisit(BaseModel):
+    url: str
+    link_count: int 
+    word_count: int 
+    image_count: int
+
+
 
 @app.get('/')
-def read_root():
-    return {"Hello": "World"}
+def save_page_visit():
+    return {'Hello': 'World'}
+
+@app.post('/api/page/visits', status_code=201)
+def save_page_visit(page_visit: PageVisit):
+    db.append({
+        **page_visit.model_dump(),
+        'created_at': datetime.now()
+    })
+    return {'status': 'ok'}
+
+
+@app.get('/api/page/visits')
+def return_page_visits(url: str):
+    return [visit for visit in db if visit['url'] == url ] 
+
+
+@app.get('/api/page/current/metrics')
+def return_page_visits(url: str):
+    filtered = [visit for visit in db if visit['url'] == url ] 
+
+    return filtered[  len(filtered)-1] 
+
--- backend/test_integration_tests.py
+++ backend/test_integration_tests.py
@@ -6,5 +6,64 @@ client = TestClient(app)
 
 def test_root():
     response = client.get('/')
-    assert response.json() == {'Hello': "you"}
+    assert response.json() == {'Hello': "World"}
+
+
+def test_save_page_visit():
+    response = client.post('/api/page/visits', json={
+        'url': 'http://dev.to/chidioguejiofor',
+        'link_count': 10,
+        'word_count': 200,
+        'image_count': 5
+    })
+    assert response.json() == {'status': 'ok'}
+    assert response.status_code == 201
+
+
+def test_retrieve_visits():
+
+    new_page_url =  'http://visit_to_retrieve.to/chidioguejiofor'
+    client.post('/api/page/visits', json={
+        'url':'http://another-url',
+        'link_count': 10,
+        'word_count': 210,
+        'image_count': 5
+    })
+
+    client.post('/api/page/visits', json={
+        'url':new_page_url,
+        'link_count': 10,
+        'word_count': 210,
+        'image_count': 5
+    })
+
+
+    response = client.get(f'/api/page/visits?url={new_page_url}')
+    res_data = response.json()
+
+    print(res_data)
+    assert len(res_data) == 1
+    page_visit = res_data[0]
+    assert page_visit is not None
+    assert response.status_code == 200
+
+def test_current_page_metrics():
+
+    new_page_url =  'http://latest_page_metrics'
+    client.post('/api/page/visits', json={
+        'url':new_page_url,
+        'link_count': 66,
+        'word_count': 880,
+        'image_count': 151
+    })
+
+
+    response = client.get(f'/api/page/current/metrics?url={new_page_url}')
+    res_data = response.json()
+
+    assert isinstance(res_data, dict)
+    res_data['url'] == new_page_url
+    res_data['word_count'] == 66
+
+
 
